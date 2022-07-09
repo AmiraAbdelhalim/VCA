6c8b217065375e4090801b88001801c54e87da0b
amira.a.a.hashim <amira.a.a.hashim@gmail.com>
2022-07-09 23:23:56 +0200

    car model dynamic list

--- tech_amb/vca/models/vca_certificates.py
+++ tech_amb/vca/models/vca_certificates.py
@@ -1,25 +1,27 @@
 from odoo import fields, models, api
+from datetime import datetime
 
 
 class VcaCertificates(models.Model):
     _name = 'vca.certificate'
     _description = 'VCA Certificate'
-    _rec_name = 'vehicle_type'
+    _rec_name = 'serial_number'
 
-    serial_number = fields.Char('Serial Number', default='/', readonly=True)
+    serial_number = fields.Char('Serial Number', default='', readonly=True)
     vehicle_type = fields.Selection([
         ('car', 'Car'),
         ('bus', 'Bus'),
         ('minibus', 'Minibus'),
         ('microbus', 'Microbus')
     ])
-    car_model = fields.Date()
+    car_model = fields.Selection([(str(year), str(year)) for year in range(datetime.now().year,
+                                                                           (datetime.now().year - 21), -1)])
     price = fields.Integer()
 
     @api.model
     def create(self, vals):
-        obj = super(VcaCertificates, self).create(vals)
-        if obj.serial_number == '/':
-            number = self.env['ir.sequence'].get('vca.sequence.code') or '/'
-            obj.write({'serial_number': number})
-        return obj
+        new_certificate = super(VcaCertificates, self).create(vals)
+        if new_certificate.serial_number == '':
+            number = self.env['ir.sequence'].get('vca.sequence.code') or ''
+            new_certificate.write({'serial_number': number})
+        return new_certificate
